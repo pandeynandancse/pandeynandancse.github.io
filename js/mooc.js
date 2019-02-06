d76017055f81c331f54bffb8c2850ea6e0de3a18
root <nandan pandey>
2019-02-07 02:19:22 +0530

    added git and datascience

--- js/mooc.js
+++ js/mooc.js
@@ -4,13 +4,31 @@ $(document).ready(function(){
     $(".web_theory").slideToggle("slow");
   });
 });
+
+
 $(document).ready(function(){
   $(".show").click(function(){
     $(".webapp_theory").slideToggle("slow");
   });
 });
+
+
 $(document).ready(function(){
   $(".showmore").click(function(){
     $(".python_theory").slideToggle("slow");
+  });
+});
+
+
+$(document).ready(function(){
+  $(".showagain").click(function(){
+    $(".data").slideToggle("slow");
+  });
+});
+
+
+$(document).ready(function(){
+  $(".showw").click(function(){
+    $(".git").slideToggle("slow");
   });
 });
